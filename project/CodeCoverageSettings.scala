0ffb0842645631d3c27a7f49f3b794f28ddbc433
Edward Oscar Taylor <48755019+ed1286@users.noreply.github.com>
2025-08-08 11:01:42 +0100

    Update CodeCoverageSettings.scala

--- project/CodeCoverageSettings.scala
+++ project/CodeCoverageSettings.scala
@@ -19,7 +19,7 @@ object CodeCoverageSettings {
   val settings: Seq[Setting[?]] = Seq(
     ScoverageKeys.coverageExcludedPackages := excludedPackages.mkString(","),
     ScoverageKeys.coverageExcludedFiles := excludedFiles.mkString(","),
-    ScoverageKeys.coverageMinimumStmtTotal := 85,
+    ScoverageKeys.coverageMinimumStmtTotal := 60,
     ScoverageKeys.coverageFailOnMinimum := true,
     ScoverageKeys.coverageHighlighting := true
   )
