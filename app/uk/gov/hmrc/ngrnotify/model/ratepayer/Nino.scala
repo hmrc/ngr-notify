60efc15ab08be204c53981c48899e0311b581649
dhinethweliwitegoda <220175817+dhinethweliwitegoda@users.noreply.github.com>
2025-10-20 17:07:14 +0100

    NGR-3143 ngr-notify: endpoint for posting ratepayer data

---
+++ app/uk/gov/hmrc/ngrnotify/model/ratepayer/Nino.scala
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrnotify.model.ratepayer
+
+import play.api.data.Form
+import play.api.data.Forms.{mapping, text}
+import play.api.libs.json.{Reads, Writes}
+import uk.gov.hmrc.domain.{SimpleObjectReads, SimpleObjectWrites, TaxIdentifier}
+import uk.gov.hmrc.ngrnotify.model.CommonFormValidators
+
+final case class Nino(nino: String) extends TaxIdentifier {
+  def value: String = nino
+}
+
+object Nino extends CommonFormValidators {
+
+  implicit val ninoWrite: Writes[Nino] = new SimpleObjectWrites[Nino](_.value)
+  implicit val ninoRead: Reads[Nino] = new SimpleObjectReads[Nino]("nino", Nino.apply)
+
+  private lazy val ninoEmptyError    = "nino.empty.error"
+  private lazy val ninoInvalidFormat = "nino.invalidFormat.error"
+  private lazy val ninoUnmatched     = "nino.unmatched.error"
+  val nino                           = "nino-value"
+
+  def unapply(nino: Nino): Option[String] = Some(nino.value)
+
+  def form(authNino: String): Form[Nino] =
+    Form(
+      mapping(
+        nino -> text()
+          .transform[String](_.toUpperCase, identity)
+          .verifying(
+            firstError(
+              isNotEmpty(nino, ninoEmptyError),
+              regexp(ninoRegexPattern.pattern(), ninoInvalidFormat),
+              isMatchingNino(authNino, nino, ninoUnmatched)
+            )
+          )
+      )(Nino.apply)(Nino.unapply)
+    )
+}
