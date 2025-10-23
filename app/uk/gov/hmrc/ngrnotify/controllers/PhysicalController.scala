6e84884845272d8df7d285698b893f6fa7edbaa5
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2025-10-23 08:10:15 +0100

    NGR-3145 POST physical changes

---
+++ app/uk/gov/hmrc/ngrnotify/controllers/PhysicalController.scala
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrnotify.controllers
+
+import play.api.Logging
+import play.api.libs.json.*
+import play.api.mvc.{Action, ControllerComponents}
+import uk.gov.hmrc.ngrnotify.connectors.HipConnector
+import uk.gov.hmrc.ngrnotify.model.bridge.{BridgeRequest, Compartments, Job}
+import uk.gov.hmrc.ngrnotify.model.propertyDetails.{PropertyChangesRequest, PropertyChangesResponse}
+import uk.gov.hmrc.play.bootstrap.backend.controller.BackendController
+import uk.gov.hmrc.ngrnotify.model.ErrorCode.*
+
+import javax.inject.Inject
+import scala.concurrent.{ExecutionContext, Future}
+
+class PhysicalController @Inject() (
+                                     hipConnector: HipConnector,
+                                     cc: ControllerComponents
+                                   )(implicit ec: ExecutionContext) extends BackendController(cc) with JsonSupport with Logging {
+  
+  def updatePropertyChanges: Action[JsValue] = Action.async(parse.json) { implicit request =>
+    request.body.validate[PropertyChangesRequest] match {
+      case JsSuccess(propertyChanges, _) =>
+        logger.info(s"Request:\n$propertyChanges")
+
+        val bridgeRequest = toBridgeRequest(propertyChanges)
+        logger.info(s"BridgeRequest:\n$bridgeRequest")
+
+        hipConnector.updatePropertyChanges(bridgeRequest).map { response =>
+            response.status match {
+              case 200 | 201 | 202 => Accepted(Json.toJsObject(PropertyChangesResponse()))
+              case 400             => BadRequest(Json.toJsObject(PropertyChangesResponse(Some(response.body))))
+              case status          => InternalServerError(buildFailureResponse(WRONG_RESPONSE_STATUS, s"$status ${response.body}"))
+            }
+          }
+          .recover(e =>
+            InternalServerError(buildFailureResponse(ACTION_FAILED, e.getMessage)))
+        
+      case jsError: JsError                => Future.successful(buildValidationErrorsResponse(jsError))
+    }
+  
+  }
+
+  private def toBridgeRequest(propertyChanges: PropertyChangesRequest): BridgeRequest =
+    BridgeRequest(
+      Job(
+        id = None,
+        idx = "?",
+        name = "physical",
+        compartments = Compartments(
+          //TODO add actual mappings when spec becomes available
+
+        )
+      )
+    )
+
+}
---
+++ test/uk/gov/hmrc/ngrnotify/backend/controllers/PhysicalControllerSpec.scala
@@ -0,0 +1,83 @@
+/*
+ * Copyright 2025 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package uk.gov.hmrc.ngrnotify.backend.controllers
+
+import org.apache.pekko.stream.Materializer
+import org.apache.pekko.stream.testkit.NoMaterializer
+import play.api.Application
+import play.api.inject.bind
+import play.api.inject.guice.GuiceApplicationBuilder
+import play.api.libs.json.Json
+import play.api.test.Helpers.*
+import play.api.test.{FakeRequest, Helpers}
+import uk.gov.hmrc.http.client.HttpClientV2
+import uk.gov.hmrc.http.{HeaderCarrier, StringContextOps}
+import uk.gov.hmrc.ngrnotify.backend.base.AnyWordAppSpec
+import uk.gov.hmrc.ngrnotify.backend.testUtils.RequestBuilderStub
+import uk.gov.hmrc.ngrnotify.controllers.PhysicalController
+import uk.gov.hmrc.ngrnotify.model.propertyDetails.{AnythingElseData, ChangeToUseOfSpace, CredId, PropertyChangesRequest}
+
+
+import java.time.LocalDate
+import scala.collection.immutable.Seq
+
+class PhysicalControllerSpec extends AnyWordAppSpec:
+
+  private val controller = inject[PhysicalController]
+
+  given Materializer = NoMaterializer
+
+  override def fakeApplication(): Application =
+    val httpClientV2Mock = mock[HttpClientV2]
+
+    when(
+      httpClientV2Mock.post(eqTo(url"http://localhost:1501/job/physical"))(using any[HeaderCarrier])
+    ).thenReturn(RequestBuilderStub(Right(OK), """"OK""""))
+
+    new GuiceApplicationBuilder()
+      .overrides(bind[HttpClientV2].to(httpClientV2Mock))
+      .build()
+
+  "PhysicalController" should {
+    ".updatePropertyChanges return 202" in {
+      val fakeRequest = FakeRequest("POST", "/")
+        .withHeaders("Content-type" -> "application/json;charset=UTF-8")
+        .withBody(Json.toJson(
+          PropertyChangesRequest(
+            CredId("credId"),
+            LocalDate.of(2023, 1, 1),
+            Some(ChangeToUseOfSpace(Seq("rearrangedTheUseOfSpace"), true, Some("REFzR42536T"))),
+            Seq(("airConditioning", "none"), ("securityCamera", "23")),
+            Seq(("loadingBays", "added"), ("lockupGarages", "removedSome")),
+            Some(AnythingElseData(true, Some("addtional text"))),
+            Seq("uploadId1", "uploadId2")
+          )
+        ))
+
+      val result = controller.updatePropertyChanges(fakeRequest)
+      status(result)          shouldBe ACCEPTED
+    }
+
+    ".updatePropertyChanges return 400" in {
+      val fakeRequest = FakeRequest("POST", "/")
+        .withHeaders("Content-type" -> "application/json;charset=UTF-8")
+
+      val result = controller.updatePropertyChanges(fakeRequest)
+      status(result) shouldBe BAD_REQUEST
+    }
+
+  }
