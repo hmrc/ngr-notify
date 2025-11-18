d688d50f73b53e7917dfa881fe6008984a0e91b3
dwalters <32767569+david-walters-HMRC@users.noreply.github.com>
2025-11-18 07:52:26 +0000

    NGR-3431 updated get status to new spec

--- app/uk/gov/hmrc/ngrnotify/connectors/HipConnector.scala
+++ app/uk/gov/hmrc/ngrnotify/connectors/HipConnector.scala
@@ -92,6 +92,11 @@ class HipConnector @Inject() (
       .get(appConfig.getPropertiesUrl(credId, assessmentId))(using hipHeaderCarrier)
       .execute[HttpResponse]
 
+  def getRatepayerStatus(id: String)(using request: Request[?]): Future[HttpResponse] =
+    httpClient
+      .get(appConfig.getRatepayerStatusUrl(id))(using hipHeaderCarrier)
+      .execute[HttpResponse]
+
   def callHelloWorld(headers: Headers): Future[HttpResponse] = {
     val url: URL = url"https://hip.ws.ibt.hmrc.gov.uk/demo/hello-world"
     httpClient
