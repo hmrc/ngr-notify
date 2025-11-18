96fd9e3c1c7f002d02dd2c4233cc710d9b1ac59c
Ashwini Rudrappa <11343826+ashwinirudrappa@users.noreply.github.com>
2025-11-18 18:19:47 +0000

    NGR-3186:Amend tests

--- test/uk/gov/hmrc/ngrnotify/model/propertyDetails/CredIdSpec.scala
+++ test/uk/gov/hmrc/ngrnotify/model/propertyDetails/CredIdSpec.scala
@@ -24,49 +24,22 @@ import play.api.mvc.PathBindable
 
 class CredIdSpec extends AnyFreeSpec {
   "CredId" - {
-    "should create an instance with the correct value" in {
-      val credIdValue = "test-cred-123"
-      val credId      = CredId(credIdValue)
-      credId.value mustEqual credIdValue
-    }
 
-    "toString should return the correct string representation" in {
-      val credIdValue = "test-cred-456"
-      val credId      = CredId(credIdValue)
-      credId.toString mustEqual credIdValue
-    }
+    "pathBindable" - {
+      "should bind a valid string to CredId" in {
+        val pathBindable = implicitly[PathBindable[CredId]]
+        val credId       = CredId("AB123")
 
-    "JSON serialization and deserialization" - {
-      import play.api.libs.json.Json
-
-      "should serialize CredId to JSON" in {
-        val credId = CredId("json-cred-123")
-        val json   = Json.toJson(credId)
-        json mustEqual JsString("json-cred-123")
+        val bind: Either[String, CredId] = pathBindable.bind("credId", "AB123")
+        bind.value mustEqual credId
       }
 
-      "should deserialize JSON to CredId" in {
-        val json   = JsString("json-cred-456")
-        val credId = json.as[CredId]
-        credId mustEqual CredId("json-cred-456")
+      "should unbind a CredId to a string" in {
+        val key            = "credId"
+        val credId         = CredId("test-cred-789")
+        val unbind: String = CredId.pathBindable.unbind(key, credId)
+        unbind mustEqual "test-cred-789"
       }
-    }
-  }
-
-  "pathBindable" - {
-    "should bind a valid string to CredId" in {
-      val pathBindable = implicitly[PathBindable[CredId]]
-      val credId       = CredId("AB123")
-
-      val bind: Either[String, CredId] = pathBindable.bind("credId", "AB123")
-      bind.value mustEqual credId
-    }
-
-    "should unbind a CredId to a string" in {
-      val key            = "credId"
-      val credId         = CredId("test-cred-789")
-      val unbind: String = CredId.pathBindable.unbind(key, credId)
-      unbind mustEqual "test-cred-789"
     }
   }
 }
